f69458467d3a07b6ae9834947638738444a6459d
Yus <yus@rateandgrade.com>
2019-06-12 18:25:04 +0200

    Initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navigation from './Navigations';
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <Navigation/>
+            </header>
+        )
+
+    }
+}
+
+export default Header;
