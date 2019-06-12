7a92e5b71889dab21bd47b1011273aa04bf013d9
Yus <yus@rateandgrade.com>
2019-06-12 23:04:16 +0200

    Added fontasome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/js/all.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import 'jquery/dist/jquery.min.js'
--- src/components/header/Navigations.js
+++ src/components/header/Navigations.js
@@ -3,7 +3,7 @@ class Navigation extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">AOTN</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -31,10 +31,11 @@ class Navigation extends Component {
                             <a className="nav-link disabled" href="#">Disabled</a>
                         </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
+                    <ul className="navbar-nav ml-auto">
+                        <li><input type="search" /></li>
+                        <li><i className="fab fa-github fa-2x"></i></li>
+                    </ul>
+                    
                 </div>
                 </nav>
         )
